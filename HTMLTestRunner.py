6a35f5efd2f1c8614bdec58a7a8378f2ac655fa4
maglweb <823358533@qq.com>
2020-08-04 18:31:18 +0800

    Initial commit

---
+++ HTMLTestRunner.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/8/4 18:30
+# @Author : Administrator
+# @Software: PyCharm
---
+++ db_fixture/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/8/4 18:27
+# @Author : Administrator
+# @Software: PyCharm
